ea9bc53c144137f8b50af41b3df48bac4a198756
Jmwalker <Jmwalker>
2017-04-29 12:44:10 -0400

    initial

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushmouth Burger', 0);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pimento Burger', 0);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Club Burger', 0);
