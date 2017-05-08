fb4fa5ceec44b90d54d55927ce11c4d187017f6e
Jmwalker <Jmwalker>
2017-05-08 14:46:02 -0400

    reset

--- db/seeds.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ('Mushmouth Burger', 0);
-
-INSERT INTO burgers (burger_name, devoured) VALUES ('Pimento Burger', 0);
-
-INSERT INTO burgers (burger_name, devoured) VALUES ('Club Burger', 0);
