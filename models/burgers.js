fa1c8dece707253d560ab4099826e2920e3e61ac
Jmwalker <Jmwalker>
2017-05-08 23:52:24 -0400

    erase

--- models/burgers.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(sequelize, DataTypes) {
-    var burger = sequelize.define("Burger", {
-        burger_name: DataTypes.STRING,
-        devoured: DataTypes.BOOLEAN,
-        date: DataTypes.DATE
-    });
-    return burger;
-}
